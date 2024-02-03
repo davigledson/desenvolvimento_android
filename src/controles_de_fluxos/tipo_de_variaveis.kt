7ce6422e829932a912a3fb09a5346bd4cb406e01
Davi Gledson <davigledson@outlook.com>
2024-02-03 19:27:25 -0300

    tipos de variáveis do kotlin

---
+++ src/controles_de_fluxos/tipo_de_variaveis.kt
@@ -0,0 +1,32 @@
+fun main() {
+var x: Int = 18 // variavel mutavel
+val y: Int = 30 // variavel Imutavel
+var a: Float = 19.2f //
+var b: Double = 3.12541 //
+var c: Long = 1010101010101010101
+var d: Char = 'F'
+var e: String = "Davi"
+var f: Short = 100
+var g: Byte = 0b1010
+var h: Byte = 0xf
+var i: UInt  = 12u //Unsigned
+
+
+
+println(x)
+println(y)
+println(b)
+println(c)
+println(d)
+println(e)
+println(f)
+println(g)
+println(h)
+println(i)
+}
+
+
+/*
+* Outra forma de comentario
+*
+* */
