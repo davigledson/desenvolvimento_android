dbf971a500614d9666c16c11336c436ea3314bf4
Davi Gledson <davigledson@outlook.com>
2024-02-03 20:50:51 -0300

    capacidades das variaveis

---
+++ src/controles_de_fluxos/capacidades_de_variaveis.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("Ulong maximo:${ULong.MAX_VALUE} Ulong Minimo:${ULong.MIN_VALUE}")
+    println("UInt maximo:${UInt.MAX_VALUE} UInt Minimo:${UInt.MIN_VALUE}")
+    println("UShort maximo:${UShort.MAX_VALUE} UShort Minimo:${UShort.MIN_VALUE}")
+    println("Long maximo:${Long.MAX_VALUE} Long Minimo:${Long.MIN_VALUE}")
+}
