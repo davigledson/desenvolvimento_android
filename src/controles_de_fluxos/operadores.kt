31788ec784d841f7ee6f2378f5584148df08fe29
Davi Gledson <davigledson@outlook.com>
2024-02-04 18:41:33 -0300

    operadores

---
+++ src/controles_de_fluxos/operadores.kt
@@ -0,0 +1,32 @@
+fun main(){
+    var h:Boolean = false
+    h = !(h)
+    println(h)
+
+    println((3+2) +5)
+
+    /**
+     * Operadores Logicos
+     *  && ->E
+     *  || -> ou
+     *  ! -> negação
+     *
+     * Operadores Matematicos
+     *
+     *  + -> Adição
+     * - -> Subtração
+     * * -> Multiplicaçao
+     * / -> Divisão
+     * % -> Resto da Divisão
+     *
+     *
+     * Operadores Comparativos
+     * > Maior que
+     * >= Maior ou igual que
+     * == igualdade voltada a comparaçao
+     *  = atribuição
+     *  != diferente
+     *
+     */
+
+}
