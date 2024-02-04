46fd9ac8cca436d4e7265d382e60de3d695d7872
Davi Gledson <davigledson@outlook.com>
2024-02-04 01:01:27 -0300

    variaveis mutaveis, imutaveis e tipos boolean

---
+++ src/controles_de_fluxos/variaveis_mutaveis_imutaveis.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var t:Int = 19
+    var str:String = "davi"
+
+
+    t++
+    str+= "Perfeito"
+
+    println("Printando número: ${t}")
+    println("Printando número: ${str}")
+
+    var B:Boolean = false
+
+    println(B)
+}
