cfac9f829a96149dd51698290d37e60d646f892d
Davi Gledson <davigledson@outlook.com>
2024-02-05 21:17:20 -0300

    while

---
+++ src/controles_de_fluxos/estrutura_de_repetição_WHILE.kt
@@ -0,0 +1,26 @@
+fun main(){
+    var i = 0
+    while (i<10){
+        print("${i} ")
+        i++
+    }
+    var o =10
+    print("\n")
+    while (o>=0){
+        print("${o} ")
+        o--
+    }
+
+    var str = "Curso show"
+    var j = 0
+    print("\n")
+    while (j<str.length){
+        print("${j} ")
+        j++
+    }
+
+    print("\n ${str[1]}")
+
+}
+
+
