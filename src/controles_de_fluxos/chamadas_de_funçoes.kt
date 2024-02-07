85e0bbe6f17983cfb2bca6e79605290a15dc7051
Davi Gledson <davigledson@outlook.com>
2024-02-07 01:56:33 -0300

    chamadas de funções

---
+++ src/controles_de_fluxos/chamadas_de_funçoes.kt
@@ -0,0 +1,27 @@
+
+fun casa(){
+    println("Entrando na casa...")
+    guarto()
+}
+fun guarto(){
+    println("Passando pelo gurda roupa")
+    guarda_roupa()
+}
+fun guarda_roupa(){
+    println("Escolhendo o sapato")
+    sapato()
+}
+
+fun sapato(){
+    println("Amarrando o cardaço")
+    cadarco()
+}
+
+fun cadarco(){
+    println("Escolhendo a cor do cadarço")
+}
+fun main(){
+    casa()
+
+}
+
