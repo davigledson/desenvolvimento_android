d0d685bf2b13c5ba388802aa2198438aeb486c20
Davi Gledson <davigledson@outlook.com>
2024-02-10 20:50:48 -0300

    leitura de dados

---
+++ src/tratamento_de_dados/leitura_de_dados.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val s = readLine() //por padrão = string
+    var num = readLine()?.toInt() ?: 0
+    var num2 = readlnOrNull()?.toInt() ?: 0
+    var num3 = readlnOrNull()?.toDouble() ?: 0.0
+    var num4 = readlnOrNull()?.toFloat() ?: 0.0f
+    var num5 = readlnOrNull()?.toBoolean() ?: false
+    println(s+1)
+}
