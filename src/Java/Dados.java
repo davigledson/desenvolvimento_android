e9ff41b22d55f93a4fc358028e8b15fd2a1a7560
Davi Gledson <davigledson@outlook.com>
2024-02-17 20:24:55 -0300

    variaveis

---
+++ src/Java/Dados.java
@@ -0,0 +1,34 @@
+package Java;
+
+import javax.swing.text.Style;
+
+public class Dados {
+    public static void main(String[] args) {
+        //tipos por referencia
+        Byte oByte = 5;
+        Short oshort = 5;
+        Integer oInteger = 5;
+        Long oLong = 10L;
+        Float oFloat = 10.0f;
+        Double oDouble = 10.0;
+        Boolean oBoolean = true;
+        String str = "teste";
+        Character c = 's';
+
+        //tipos primitivos
+        byte aByte = 5;
+        short ashort = 5;
+        int aInteger = 5;
+        long aLong = 10L;
+        float aFloat = 10.0f;
+        double aDouble = 10.0;
+        boolean aBoolean = true;
+        str = "teste";
+        char ac = 's';
+
+        System.out.println(oByte.toString());
+        System.out.println(aByte);
+
+
+    }
+}
