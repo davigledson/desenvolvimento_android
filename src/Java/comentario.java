40fe4edd9b79de0d4bc42d188e5ddd00e5b57044
Davi Gledson <davigledson@outlook.com>
2024-02-17 02:53:52 -0300

    iniciando em dev com java

---
+++ src/Java/comentario.java
@@ -0,0 +1,14 @@
+package Java;
+
+/**
+ * JavaDoc - Comentario voltado a documentação de arquivo
+ */
+public class comentario {
+    public static void main(String[] args) {
+        //Comentario somente em linha
+        System.out.println();
+    }
+}
+/*
+* Comentario em bloco
+* */
