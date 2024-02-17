fc29639be99cd1793fe3f0f4ba6bea66f5c359b8
Davi Gledson <davigledson@outlook.com>
2024-02-17 00:25:23 -0300

    iniciando estudo em java

---
+++ src/Java/Programa.java
@@ -0,0 +1,9 @@
+package Java;
+//só pode ter uma unica classe em um msm arquivo
+//o nome da classe tem que ser o msm nome do arquivo
+public class Programa {
+    public static void main(String[] args) {
+
+        System.out.println("Olá mundo!");
+    }
+}
