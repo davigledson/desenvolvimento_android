e05f7f83e2e9b9ece817d99789354b61e9bdec3a
Davi Gledson <davigledson@outlook.com>
2024-02-17 23:22:07 -0300

    precedência

---
+++ src/Java/Prioridadades_PO.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class Prioridadades_PO {
+    public static void main(String[] args) {
+        int x = 10/2*3+3*4;
+        //Precendencia dos sinais
+        //divisão
+        //multiplicaocao
+        //resto da divisão
+        //adicão e subtração
+        System.out.println(x);
+
+        int a  =6*(6*5);
+        System.out.println(a);
+    }
+}
