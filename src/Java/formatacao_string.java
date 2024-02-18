75fabd7c3ff31fe71a8c1d2b8ed221e4c3a30e8f
Davi Gledson <davigledson@outlook.com>
2024-02-18 00:26:41 -0300

    formatação de string

---
+++ src/Java/formatacao_string.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class formatacao_string {
+    public static void main(String[] args) {
+        String a = "Ola";
+        String b = "Davi";
+        String c = "gfdklgjdfklgjdfklgjdfklgjdfklgjdfk" +
+                "lgjdfklgjdfklgjdfklgj" +
+                "dfklgdjgkdlgjdkflgjfdklgjdkgjfdklgfdjlkg";
+        System.out.println(a + b);
+
+        System.out.println("Testando o meu " +
+                "\nsoftware de variaas" +
+                " formas");
+
+        System.out.println("ela me disse 'ola'");
+        System.out.println("ela me disse \"ola\"");
+    }
+}
