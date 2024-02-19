db9075c3e505deee68709b83c58bd694cd99cc52
Davi Gledson <davigledson@outlook.com>
2024-02-18 22:10:59 -0300

    funções Math

---
+++ src/Java/Math_funcao.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class Math_funcao {
+    public static void main(String[] args) {
+        System.out.println(Math.max(0,20));
+        System.out.println(Math.min(0,20));
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.log10(100.0));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.round(2000.245462));
+        System.out.println(Math.random());
+    }
+}
