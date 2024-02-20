68d51225be9931568218c3170b7e4180d8035804
Davi Gledson <davigledson@outlook.com>
2024-02-20 06:46:23 -0300

    enhaced

---
+++ src/Java/Enhaced.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class Enhaced {
+    public static void main(String[] args) {
+        int n [] = {1,2,3,4,5,6,7,8,9};
+        for(int v: n){
+            System.out.print(v + " ");
+        }
+
+        System.out.println();
+        char c[] = {'c','d','h','g','e','y'};
+        for(char v: c ){
+            System.out.print(v+ " ");
+        }
+    }
+}
