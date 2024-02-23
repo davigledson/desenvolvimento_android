b9453267dc8949b79d5f5e8390cead3c4b01e350
Davi Gledson <davigledson@outlook.com>
2024-02-23 02:08:39 -0300

    Construtores

---
+++ src/Java_PDO/Construtor.java
@@ -0,0 +1,19 @@
+package Java_PDO;
+
+public class Construtor {
+    public Integer idade;
+    public Float saldo;
+    public String nome;
+
+    public Construtor(String h){
+        idade=10;
+        saldo=20.0f;
+        nome = "Benedito";
+    }
+
+    public Construtor(){
+        idade=0;
+        saldo=0.0f;
+        nome = " ";
+    }
+}
---
+++ src/Java_PDO/Main_Construtor.java
@@ -0,0 +1,18 @@
+package Java_PDO;
+
+public class Main_Construtor {
+    public static void main(String[] args) {
+        new Construtor("Davi");
+        Construtor c = new Construtor();
+        Construtor c2 = new Construtor("Slva");
+
+        System.out.println(c.idade);
+        System.out.println(c.nome);
+        System.out.println(c.saldo);
+
+        System.out.println(c2.idade);
+        System.out.println(c2.nome);
+        System.out.println(c2.saldo);
+    }
+
+}
