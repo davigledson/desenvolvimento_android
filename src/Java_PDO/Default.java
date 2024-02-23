94b68a50d914f0a580e7d8d5bad9865fe8972a22
Davi Gledson <davigledson@outlook.com>
2024-02-22 21:56:43 -0300

    Valores Default

---
+++ src/Java_PDO/Default.java
@@ -0,0 +1,13 @@
+package Java_PDO;
+
+public class Default {
+    public static void main(String[] args) {
+        OBJ_Default d = new OBJ_Default();
+        System.out.println("Nome: " +d.nome);
+        System.out.println("numero: " +d.numero);
+        System.out.println("Float: " +d.f);
+        System.out.println("Bolean: " +d.b);
+        System.out.println("Double: " +d.d);
+        System.out.println("Numero referencial (colocando o zero por padrão): " + d.obj);
+    }
+}
---
+++ src/Java_PDO/OBJ_Default.java
@@ -0,0 +1,12 @@
+package Java_PDO;
+
+public class OBJ_Default {
+    public  Integer nome;
+    public Integer numero;
+    public Integer obj=0;
+
+
+    public  double d; //tipos primitivos tem o valor 0 por padrão
+    public float f;
+    public boolean b;
+}
